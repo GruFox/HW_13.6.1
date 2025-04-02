80fddf58690bf8967cc30be549a103580b639f3b
Gru <energimen@yandex.ru>
2025-04-02 09:20:55 +0200

    Добавьте файлы проекта.

---
+++ HW_13.6.1_LinkedList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace HW_13._6._1_LinkedList;
+
+internal class Program
+{
+    static string text = File.ReadAllText("C:\\Users\\777\\Desktop\\GruFox\\C#\\HW_13.6.1\\input.txt");
+
+    static void Main(string[] args)
+    {
+        LinkedList<string> linkedList = new LinkedList<string>();
+
+        // Сохраняем символы-разделители в массив
+        char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+        // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+        var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var word in words)
+        {
+            linkedList.AddLast(word);
+        }
+
+        // запускаем новый таймер
+        var stopWatch = Stopwatch.StartNew();
+
+        linkedList.AddAfter(linkedList.First, "MIDDLE");
+
+        // смотрим, сколько операция заняла, в миллисекундах
+        Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds + "\n");
+    }
+}
+
---
+++ HW_13.6.1_List/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace HW_13._6._1_List;
+
+internal class Program
+{
+    static string text = File.ReadAllText("C:\\Users\\777\\Desktop\\GruFox\\C#\\HW_13.6.1\\input.txt");
+
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+
+        // Сохраняем символы-разделители в массив
+        char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+        // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+        var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        list.AddRange(words);
+
+        // запускаем новый таймер
+        var stopWatch = Stopwatch.StartNew();
+
+        list.Insert(1, "MIDDLE");
+
+        // смотрим, сколько операция заняла, в миллисекундах
+        Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds + "\n");
+    }
+}
